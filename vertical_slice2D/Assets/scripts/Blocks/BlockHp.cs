1b395ce14e3ff39bae2a28ca079b803c326fa02f
blizzard190 <22256@ma-web.nl>
2018-01-22 16:20:16 +0100

    Block switch
    block to damaged block

---
+++ vertical_slice2D/Assets/scripts/Blocks/BlockHp.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockHp : MonoBehaviour {
+    private float hp = 2;
+    private SpriteRenderer spR;
+    //[SerializeField]
+    public Sprite damage;
+
+    private void Start()
+    {
+        spR = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            Debug.Log("touch" + hp);
+            hp -= 1;
+        }
+    }
+    private void Update()
+    {
+        if(hp == 1)
+        {
+            spR.sprite = damage;
+        }
+        else if (hp <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
